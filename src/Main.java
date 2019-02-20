2c0bed1660d80176d6d4b4b4e6e7b71a81c4c314
Gruszkowski <Gruszkowski@users.noreply.github.com>
2019-02-20 17:50:27 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+
+        System.out.println();
     }
+
+
+
+
+
+    //siemaaneczko
 }
 
 
+///Siemanko
